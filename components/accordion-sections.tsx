101a9b066889013e8af27c19befa505c976338ae
Il Tuo Nome <vincenzopetronebiz@gmail.com>
2025-10-17 12:17:54 +0200

    Revert carousel radius back to 750px for better spacing

--- components/accordion-sections.tsx
+++ components/accordion-sections.tsx
@@ -336,7 +336,7 @@ export function AccordionSections() {
                   {t(section.contentKey)}
                 </p>
                 <div className="w-full h-[500px] md:h-[600px]">
-                  <CircularGallery items={galleryItems} radius={550} autoRotateSpeed={0.015} />
+                  <CircularGallery items={galleryItems} radius={750} autoRotateSpeed={0.015} />
                 </div>
                 {section.cta && section.cta.href && (
                   <div className="mt-8 flex justify-center">
